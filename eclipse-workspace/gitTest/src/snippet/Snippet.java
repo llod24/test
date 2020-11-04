43ec4e0ee40012e6a433a334face2b05b20b343c
최형인 <@DESKTOP-6B7HJ72>
2020-11-04 22:16:20 +0900

    all commit

---
+++ eclipse-workspace/gitTest/src/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	echo "# termproject" >> README.md
+	git init
+	git add README.md
+	git commit -m "first commit"
+	git branch -M main
+	git remote add origin https://github.com/llod24/termproject.git
+	git push -u origin main
+	                
+}
+
